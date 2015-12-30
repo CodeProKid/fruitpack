a950349dd0196c4f2f3acd4e0db100cadc1f240a
Ryan Kanner <ryan.kanner@fruition.net>
2015-12-30 10:07:52 -0700

    adding info to plugin header for sitemap module

--- modules/sitemap/index.php
+++ modules/sitemap/index.php
@@ -4,6 +4,11 @@
  * Description: Generates sitemap list through shortcode
  * Slug: sitemap
  * Icon: dashicons-admin-site
+ * Version: 1.0.0
+ * Author: Ryan Kanner
+ * Author URI: http://rkanner.com
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require_once( dirname(__FILE__) . '/class.register.php' );
