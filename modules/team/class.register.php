c9d565af14afc5b01f26c82af76658df3e09e8d1
Ryan Kanner <ryan.kanner@fruition.net>
2016-02-01 10:39:29 -0700

    adding filters for registering team cpt and category for teams

--- modules/team/class.register.php
+++ modules/team/class.register.php
@@ -22,7 +22,7 @@ class FruitpackTeam {
 	*/
 	public static function team() {
 	
-		$labels = array(
+		$labels = apply_filters( 'fp_register_team_labels', array(
 			'name'                => __( 'Team Members', 'fruitpack' ),
 			'singular_name'       => __( 'Team Member', 'fruitpack' ),
 			'add_new'             => _x( 'Add New Team Member', 'fruitpack', 'fruitpack' ),
@@ -35,9 +35,9 @@ class FruitpackTeam {
 			'not_found_in_trash'  => __( 'No Team Members found in Trash', 'fruitpack' ),
 			'parent_item_colon'   => __( 'Parent Team Member:', 'fruitpack' ),
 			'menu_name'           => __( 'Team Members', 'fruitpack' ),
-		);
+		) );
 	
-		$args = array(
+		$args = apply_filters( 'fp_register_team_args', array(
 			'labels'              => $labels,
 			'hierarchical'        => false,
 			'description'         => 'Team CPT',
@@ -57,7 +57,7 @@ class FruitpackTeam {
 			'rewrite'             => true,
 			'capability_type'     => 'post',
 			'supports'            => array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'page-attributes' )
-		);
+		) );
 	
 		register_post_type( 'team', $args );
 
@@ -76,24 +76,24 @@ class FruitpackTeam {
 	 */
 	public static function team_category() {
 	
-		$labels = array(
-			'name'					        => _x( 'Team Categories', 'Taxonomy plural name', 'fruitpack' ),
-			'singular_name'			    => _x( 'Team Category', 'Taxonomy singular name', 'fruitpack' ),
-			'search_items'			    => __( 'Search Team Categories', 'fruitpack' ),
-			'popular_items'			    => __( 'Popular Team Categories', 'fruitpack' ),
-			'all_items'				      => __( 'All Team Categories', 'fruitpack' ),
-			'parent_item'			      => __( 'Parent Team Category', 'fruitpack' ),
-			'parent_item_colon'		  => __( 'Parent Team Category', 'fruitpack' ),
-			'edit_item'				      => __( 'Edit Team Category', 'fruitpack' ),
-			'update_item'			      => __( 'Update Team Category', 'fruitpack' ),
-			'add_new_item'			    => __( 'Add New Team Category', 'fruitpack' ),
-			'new_item_name'			    => __( 'New Team Category Name', 'fruitpack' ),
-			'add_or_remove_items'	  => __( 'Add or remove Team Categories', 'fruitpack' ),
+		$labels = apply_filters( 'fp_register_team_category_labels', array(
+			'name'					=> _x( 'Team Categories', 'Taxonomy plural name', 'fruitpack' ),
+			'singular_name'			=> _x( 'Team Category', 'Taxonomy singular name', 'fruitpack' ),
+			'search_items'			=> __( 'Search Team Categories', 'fruitpack' ),
+			'popular_items'			=> __( 'Popular Team Categories', 'fruitpack' ),
+			'all_items'				=> __( 'All Team Categories', 'fruitpack' ),
+			'parent_item'			=> __( 'Parent Team Category', 'fruitpack' ),
+			'parent_item_colon'		=> __( 'Parent Team Category', 'fruitpack' ),
+			'edit_item'				=> __( 'Edit Team Category', 'fruitpack' ),
+			'update_item'			=> __( 'Update Team Category', 'fruitpack' ),
+			'add_new_item'			=> __( 'Add New Team Category', 'fruitpack' ),
+			'new_item_name'			=> __( 'New Team Category Name', 'fruitpack' ),
+			'add_or_remove_items'	=> __( 'Add or remove Team Categories', 'fruitpack' ),
 			'choose_from_most_used'	=> __( 'Choose from most used fruitpack', 'fruitpack' ),
-			'menu_name'				      => __( 'Team Category', 'fruitpack' ),
-		);
+			'menu_name'			    => __( 'Team Category', 'fruitpack' ),
+		) );
 	
-		$args = array(
+		$args = apply_filters( 'fp_register_team_category', array(
 			'labels'            => $labels,
 			'public'            => true,
 			'show_in_nav_menus' => true,
@@ -105,7 +105,7 @@ class FruitpackTeam {
 			'rewrite'           => false,
 			'query_var'         => true,
 			'capabilities'      => array(),
-		);
+		) );
 	
 		register_taxonomy( 'team-category', array( 'team' ), $args );
 
