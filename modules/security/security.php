ab4c32479822e6d1c0124addc2dd7c73bd8c2162
Ryan Kanner <ryan@codeprostudios.com>
2016-02-03 21:38:49 -0700

    adding code to stop user enumeration

--- modules/security/security.php
+++ modules/security/security.php
@@ -43,3 +43,32 @@ remove_action('wp_head', 'wp_generator');
 if ( !defined('DISALLOW_FILE_EDIT') ) {
 	define( 'DISALLOW_FILE_EDIT', true );
 }
+
+if ( ! is_admin()){
+  if(preg_match('/(wp-comments-post)/', $_SERVER['REQUEST_URI']) === 0 ) {
+     if (!empty($_POST['author'])) {
+        fp_kill_enumeration();
+     }
+  }
+
+  if(preg_match('/author=([0-9]*)/', $_SERVER['QUERY_STRING']) === 1)
+  fp_kill_enumeration();
+
+  add_filter('redirect_canonical','fp_detect_enumeration', 10,2);
+}
+
+add_filter('redirect_canonical','fp_detect_enumeration', 10,2);
+function fp_detect_enumeration ($redirect_url, $requested_url) {
+if (preg_match('/\?author(%00[0%]*)?=([0-9]*)(\/*)/', $requested_url)===1  | isset($_POST['author']) ) {
+     fp_kill_enumeration();
+   } else {
+     return $redirect_url;
+   }
+}
+
+function fp_kill_enumeration() {
+     openlog('wordpress('.$_SERVER['HTTP_HOST'].')',LOG_NDELAY|LOG_PID,LOG_AUTH);
+     syslog(LOG_INFO,"Attempted user enumeration from {$_SERVER['REMOTE_ADDR']}");
+     closelog();
+     wp_die('forbidden');
+}
